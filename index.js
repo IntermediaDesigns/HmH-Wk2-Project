27c8ffedfd85baf2c69f99f5eb94234418522c27
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-09 16:30:40 -0500

    2

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 // A word is chosen at random from a pool of, at minimum, five words.
-function randomWords (words){
-       let output = " ";
+let userGuess = true;
+let letters = /a-z/
+let guesses = 3;
+let wrongGuess;
+let playAgain = true;
 
-       for(let i = 0; i < words; i++);{
-              console.log(math.ceil(math.random()* 6));
-       }
+// Random Words
+
+
+// Start Game - Pick random word
+while (playing) {
+       starting = prompt(
+              "I'm thinking of a word. Would you like to guess a letter?"
+            );
+            let word = words[Math.floor(Math.random() * words.length)];
+
+            let answer = [];
+              for (var i = 0; i < word.length; i++) {answer[i] = "_";}
+
+              if (starting === "word") {word();}
+                     else if (starting === "javascript") {javascript();}
+                     else if (starting === "object") {object();}
+                     else if (starting === "boolean") {boolean();}
+                     else if (starting === "variable") {variable();}
+                     else if (starting === "waffles") {waffles();}
 }
-console.log(output.length);
-return output;
 
-// I'm thinking of a word. Would you like to guess a letter?
+// Words
+function word() {
+       let firstWord = prompt("Enter the first number:");
+       let secondWord = prompt("Enter the second number:");
+       let thirdWord = prompt("Enter the second number:");
+       let fourthWord = prompt("Enter the second number:");
+       let fifthWord = prompt("Enter the second number:");
+       let sixthWord = prompt("Enter the second number:");
+       
+       let guess = firstWord * secondWord;
+              alert(`${firstNumber} * ${secondNumber} = ${guess}`)
+}
+
+
+
+
+// Player guessing
+let remainingLetters = word.length;
+while (remainingLetters > 0) {
+       alert(answer.join("_"));
+}
+
+
+// Player guess correct
+function initiateGuess(){
+       let correct = chosenWord.match(userSelection);
+}
+
+
+
+
 
 // The following characters are automatically populated in the chosen random word: 'r', 's', 't', 'l', 'n', 'e'.
 
@@ -21,6 +68,11 @@ return output;
 
 
 // If the user gets three incorrect guesses, they lose.
+
+function incorrect(){
+       incorrectGuess =  wrongGuess.push(userGuess);
+       return incorrectGuess;
+       }
 
 // You Lost!
 
