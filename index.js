b2064b9aa135cc51a3cf5218da700719f6eeb20a
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-09 14:47:44 -0500

    2

---
+++ index.js
@@ -0,0 +1,43 @@
+// A word is chosen at random from a pool of, at minimum, five words.
+function randomWords (words){
+       let output = " ";
+
+       for(let i = 0; i < words; i++);{
+              console.log(math.ceil(math.random()* 6));
+       }
+}
+console.log(output.length);
+return output;
+
+// I'm thinking of a word. Would you like to guess a letter?
+
+// The following characters are automatically populated in the chosen random word: 'r', 's', 't', 'l', 'n', 'e'.
+
+
+// A player types in a letter as a guess, if they are correct the letter is filled in and if they are incorrect one rope emoji appears in the prompt.
+
+
+// If the user guesses all the correct letters, they win.
+
+
+// If the user gets three incorrect guesses, they lose.
+
+// You Lost!
+
+
+
+
+
+
+
+
+/*  3 Wrong guesses.
+
+    First wrong answer: head and post ☹⅂
+
+    Second: body  \|／ /\
+
+    Third: "Hangman!”  𖨆⅂
+
+    
+*/
